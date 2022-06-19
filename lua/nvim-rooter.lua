de8d11a1799cd5c2cc27e8ec724d96a0e3469d73
liqingshuo <1905488733@qq.com>
2022-06-19 17:30:20 +0800

    compatible with alpha

--- lua/nvim-rooter.lua
+++ lua/nvim-rooter.lua
@@ -52,20 +52,21 @@ end
 local function get_root()
   -- don't need to resove sybolic links explicitly, because
   -- `nvim_buf_get_name` returns the resolved path.
-  local current = vim.api.nvim_buf_get_name(0)
-  local parent = parent_dir(current)
+  local search_dir = vim.fn.expand("%:p:h", true)
+  vim.api.nvim_buf_set_var(0, 'current_dir', search_dir)
 
   while 1 do
     for _, pattern in ipairs(_config.patterns) do
-      if match(parent, pattern) then
-        return parent
+      if match(search_dir, pattern) then
+        return search_dir
       end
     end
 
-    current, parent = parent, parent_dir(parent)
-    if parent == current then
+    local parent = parent_dir(search_dir)
+    if parent == search_dir then
       break
     end
+    search_dir = parent
   end
   return nil
 end
@@ -87,7 +88,8 @@ local function rooter()
 end
 
 local function rooter_toggle()
-  local parent = parent_dir(vim.api.nvim_buf_get_name(0))
+  local parent = vim.fn.exists('b:current_dir') == 1 and vim.api.nvim_buf_get_var(0, 'current_dir')
+                  or parent_dir(vim.api.nvim_buf_get_name(0))
   if vim.fn.getcwd() ~= parent then
     change_dir(parent)
   else
